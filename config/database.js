7ffa88b29d03be31742f23a96bf40c42dbfafe26
Shaggy Mission <rescuefriendsuio@gmail.com>
2025-06-24 08:54:17 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('role_service_db', 'admin', 'PoolG10.11',
+const sequelize = new Sequelize('role_service_db', 'admin', 'Lis.12345',
   {
-    host: 'mariadb.cwizczqy6pft.us-east-1.rds.amazonaws.com',
+    host: 'shaggymission.cd5kwh1iyrrb.us-east-1.rds.amazonaws.com',
     dialect: 'mariadb',
     logging: false,
   }
